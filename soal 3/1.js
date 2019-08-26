7cf8a53ad50daa9c2f7e8602b02427dcf85c6f52
nurmarf <45351487+nurmarf@users.noreply.github.com>
2019-08-26 16:08:34 +0700

    Create 1.js

---
+++ soal 3/1.js
@@ -0,0 +1,7 @@
+function addTitle(person1, person2, person3, person4){
+    var a = ' Mr. ';
+    var b = ',';
+    console.log(a + person1 + b + a + person2 + b + a + person3 + b + a + person4)
+  }
+  
+  addTitle("Stave Fox", "Jin Kazama", 'Eddie Gordo', 'Geese Howard')
